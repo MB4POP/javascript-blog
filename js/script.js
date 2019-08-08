d676c12489c04ad9685dd849270e32a980c6e450
Mariusz Białecki <mariusz.bialecki@onet.pl>
2019-08-08 14:41:13 +0200

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@
 }); */
 
 const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -16,9 +18,12 @@ const titleClickHandler = function(event){
         activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.post.active');
 
